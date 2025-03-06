56b1210650258cda88d03d87842c950b37bb67c1
basnaly <basnaly@gmail.com>
2025-03-06 20:07:06 +0200

    - add reducer instead of state - delete workout

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,93 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
 import Sidebar from './components/Sidebar'
 import Starting from './components/Srarting'
 
 const storedWorkouts = JSON.parse(localStorage.getItem("workouts")) || [];
 
-function App() {
+// initialState = {
+// 	workouts: [
+// 	{
+// 		id: "",
+// 		date: "",
+// 		workout: "",
+// 		duration: "",
+// 		feelings: ""
+// 	}
+// ]
 
-	const [workouts, setWorkouts] = useState(storedWorkouts)
-		
-		// [{
-		// 	id: "",
-		// 	date: "",
-		// 	workout: "",
-		// 	duration: "",
-		// 	feelings: ""
-		// }]
-	
-	function addNewWorkout(inputData) {
-		setWorkouts((prevState) => (
-			[
-				...prevState,
-				inputData
-			]
-		))
+
+function workoutsReducer(state, action) {
+
+	if (action.type === "ADD_WORKOUT") {
+
+		const storedWorkouts = JSON.parse(localStorage.getItem("workouts")) || [];
+		const existingId = storedWorkouts.find((el) => el.id === action.payload.id);
+
+		if (!existingId) {
+			localStorage.setItem(
+				"workouts",
+				JSON.stringify([action.payload, ...storedWorkouts])
+			)
+		}
+		return {
+			...state,
+			workouts: [action.payload, ...state.workouts]
+		}
+	}
+
+	if (action.type === "DELETE_WORKOUT") {
+
+		const updatedWorkouts = [...state.workouts].filter((workout) => workout.id !== action.payload);
 
 		const storedWorkouts = JSON.parse(localStorage.getItem("workouts")) || [];
 		localStorage.setItem(
 			"workouts",
-			JSON.stringify([inputData, ...storedWorkouts])
-		)
+			JSON.stringify(storedWorkouts.filter((el) => el.id !== action.payload)
+		))
+
+		return {
+			...state,
+			workouts: updatedWorkouts
+		}
 	}
 
-	console.log(workouts)
+	return state;
+}
+
+
+function App() {
+
+	const [workoutsState, workoutsDispatch] = useReducer(
+		workoutsReducer,
+		{
+			workouts: storedWorkouts,
+		}
+	)
+
+	function addWorkout(inputData) {
+		workoutsDispatch({
+			type: "ADD_WORKOUT",
+			payload: inputData
+		})
+	}
+
+	function deleteWorkout(id) {
+		workoutsDispatch({
+			type: "DELETE_WORKOUT",
+			payload: id
+		})
+	}
+
+
+	console.log(workoutsState.workouts)
 
 	return (
 		<main className='h-screen flex'>
-			<Sidebar workouts={workouts} />
-			<Starting  onAdd={addNewWorkout} />
+			<Sidebar 
+				workouts={workoutsState.workouts} 
+				onDelete={deleteWorkout}
+			/>
+			<Starting onAdd={addWorkout} />
 
 		</main>
 	)
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,27 +1,42 @@
 
-export default function Sidebar({ workouts }) {
+export default function Sidebar({ workouts, onDelete }) {
+
+    console.log(workouts)
 
     return (
-        <aside className="w-1/3 bg-blue-200">
-            <h2 className="text-2xl my-16 ms-8">
+        <aside className="w-1/3 bg-blue-200 overflow-auto flex flex-col">
+            <h2 className="text-2xl m-8">
                 My workouts
             </h2>
-            {workouts.length === 0 && <p>You don't have workouts yet!</p> }
+            {workouts.length === 0 && (
+                <p className="ms-8 text-[1.1rem]">
+                    You don't have workouts yet!
+                </p>
+            )}
             {workouts.length > 0 && (
-                <div className="m-3">
+                <ul className="my-3 overflow-auto">
                     {workouts.map((workout) => (
-                        <ul className="m-4"
-                            key={workout.id}
+                        <li className="m-4 flex items-center"
+                            key={workout?.id}
                         >
-                            <li>{workout.date}</li>
-                            <li className="text-[1rem] font-bold">
-                                {workout.workout.toUpperCase()}
-                            </li>
-                            <li>Duration: {workout.duration}</li>
-                            <li>{workout.fillings}</li>
-                        </ul>
+                            <div>
+                                <div>{workout?.date}</div>
+                                <div className="text-[1rem] font-bold">
+                                    {workout?.workout?.toUpperCase()}
+                                </div>
+                                <div>Duration: {workout?.duration}</div>
+                                <div>{workout?.fillings}</div>
+                            </div>
+
+                            <button 
+                                onClick={() => onDelete(workout.id)}
+                                className="ms-5 px-3 py-1 md:text-base rounded-md bg-stone-100 text-red-600 hover:bg-stone-200 hover:text-red-700">
+                                Delete
+                            </button>
+                        </li>
+
                     ))}
-                </div>
+                </ul>
             )}
 
         </aside>
